4c29d8b5205b910ffe3aae9779c17fa9763b2b23
dmitche <doug.mitchell@Doug-Mitchell-2.local>
2016-01-28 12:45:26 -0600

    1st try using formspree.io

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,14 +18,17 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                //url: "././mail/contact_me.php",
+                url: "//formspree.io/dmitche@gmail.com",
+                method: "POST",
+                //type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
+                dataType: "json"
                 cache: false,
                 success: function() {
                     // Success message
