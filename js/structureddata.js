e2f24bd974fd071dca043fe524a5cb9fce52733a
Doug Mitchell <mitchell@Mitchell-MacBook-Pro.local>
2018-03-13 21:34:24 -0500

    Add Structured Data JS

---
+++ js/structureddata.js
@@ -0,0 +1,40 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "LocalBusiness",
+  "image": [
+    "http://jts.services/img/header-bg-light.jpg"
+   ],
+  "@id": "http://jts.services",
+  "name": "JTS Power Washing Services",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "973 Wagon Wheel Rd",
+    "addressLocality": "Springdale",
+    "addressRegion": "AR",
+    "postalCode": "72764",
+    "addressCountry": "US"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 36.224664,
+    "longitude": -94.1405952
+  },
+  "url": "http://jts.services",
+  "telephone": "+14792634706",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      "opens": "8:00",
+      "closes": "17:00"
+    }
+  ]
+}
+</script>
