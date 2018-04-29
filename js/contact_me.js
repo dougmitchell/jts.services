98b386ea1dc50c1a70b0cdc85d0bb3acb1df10e0
Doug Mitchell <mitchell@Mitchell-MacBook-Pro.local>
2018-04-28 22:54:06 -0500

    Try to Add GA Event on Contact us

--- js/contact_me.js
+++ js/contact_me.js
@@ -34,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you for contacting JTS Services. We will contact you soon. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -51,6 +51,9 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
+                ga('send', {
+  eventAction: 'Send Message'
+}),	
             })
         },
         filter: function() {
