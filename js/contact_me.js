b15abe41218e8c27cf80b863fb86f84556e84d9c
Doug Mitchell <mitchell@Mitchell-MacBook-Pro.local>
2018-04-28 23:09:16 -0500

    Revert "Try to Add GA Event on Contact us"
    This reverts commit 98b386ea1dc50c1a70b0cdc85d0bb3acb1df10e0.

--- js/contact_me.js
+++ js/contact_me.js
@@ -34,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thank you for contacting JTS Services. We will contact you soon. </strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -51,9 +51,6 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                ga('send', {
-  eventAction: 'Send Message'
-}),	
             })
         },
         filter: function() {
